f90e0db5d1f2be6a3bb26da899feeb7a88c72eea
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-18 14:06:06 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,40 +1,18 @@
-//init ably
 const realtime = new Ably.Realtime({
   key: 'x44j9Q.0J9W1Q:fF7ThTZMPZrHy88z2RUe0TCAc3Qr94Ccn5Bs8GPfl64'
 });
 
-console.log(realtime.clientId); // Output: A unique client ID assigned by Ably
-const clientId = realtime.clientId
+realtime.connection.once('connected', () => {
+  console.log(realtime.clientId); // Output: A unique client ID assigned by Ably
+});
 
-const ably = new Ably.Realtime({ key: 'x44j9Q.0J9W1Q:fF7ThTZMPZrHy88z2RUe0TCAc3Qr94Ccn5Bs8GPfl64', clientId: clientId});
-const chatClient = new ChatClient(ably, {logHandler: logWriteFunc, logLevel: 'debug' });
+const chatClient = new ChatClient(realtime, { logHandler: logWriteFunc, logLevel: 'debug' });
 
-
-const connectionStatus = chatClient.connection.status.current;
-console.log(connectionStatus);
-
-
-const { off } = chatClient.connection.status.onStatusChange((change) => console.log(change));
-
-// terminate connection off();
-
-// terminate all connections chatClient.connection.status.offAll();
-
-// get main room
 const room = chatClient.rooms.get('main');
 
-// attatch this user
-await room.attach();
-
-// subscribe to main room messages
-
-const {unsubscribe} = room.messages.subscribe(message) => {
-  console.log(message);
-}
-
-const messageList = document.getElementById('chat-log');
-
-room.messages.subscribe((message) => {
-  const messageHTML = `<p>${message.data}</p>`;
-  messageList.insertAdjacentHTML('beforeend', messageHTML);
+room.attach().then(() => {
+  room.messages.subscribe((message) => {
+    const messageHTML = `<p>${message.data}</p>`;
+    messageList.insertAdjacentHTML('beforeend', messageHTML);
+  });
 });
