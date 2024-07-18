41142614a957c37c6657bea5acaf5081ed907975
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-18 13:35:40 +0800

    Create main.js

---
+++ src/main.js
@@ -0,0 +1,16 @@
+//init ably
+
+const ably = new Ably.Realtime({ key: 'x44j9Q.0J9W1Q:fF7ThTZMPZrHy88z2RUe0TCAc3Qr94Ccn5Bs8GPfl64', clientId: '<clientId>'});
+const chatClient = new ChatClient(ably, {logHandler: logWriteFunc, logLevel: 'debug' });
+
+
+const connectionStatus = chatClient.connection.status.current;
+console.log(connectionStatus);
+
+
+const { off } = chatClient.connection.status.onStatusChange((change) => console.log(change));
+
+// terminate connection off();
+
+// terminate all connections chatClient.connection.status.offAll();
+
