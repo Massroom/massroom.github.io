82c0312f7b0de44db6ad878a34ea2b214269d19a
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-20 22:54:20 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -116,11 +116,12 @@ function displayMessage(message) {
   messageList.scrollTop = messageList.scrollHeight;
 }
 
-const userList = mainChannel.getPresence();
-const userCount = userList.length;//.length;
-console.log(userList);
-document.getElementById('user-count').innerHTML = `Users: ${userCount}`;
-
+mainChannel.getPresence().then((presence) => {
+  const userList = presence.users;
+  const userCount = userList.length;
+  console.log(userList);
+  document.getElementById('user-count').innerHTML = `Users: ${userCount}`;
+});
 
 sendButton.addEventListener('click', () => {
   const message = messageInput.value;
