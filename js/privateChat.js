ce5064d11df91bf0186dff3bc84f2f7f549acfd7
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-20 14:06:06 +0800

    Update privateChat.js

--- js/privateChat.js
+++ js/privateChat.js
@@ -1,23 +1,8 @@
-class checkAuth {
-     // setup the class and hide the body by default
-    constructor() {
-        document.querySelector("body").style.display = "none";
-        const auth = localStorage.getItem("auth");
-        this.validateAuth(auth);
-    }
-    // check to see if the localStorage item passed to the function is valid and set
-    validateAuth(auth) {
-        if (auth != 1) {
-            window.location.replace("/authuser");
-        } else {
-            document.querySelector("body").style.display = "block";
-        }
-    }
-}
+var auth = localStorage.getItem("auth");
 
-const auth = new checkAuth();
-
-
+if (auth != 1) {
+      window.location.replace("/authuser");
+ }
 
 // private chat
 
