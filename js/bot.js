d0a1857a7a80f94f5875bd3942af064acfe5e38c
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-21 16:06:36 +0800

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -3,3 +3,18 @@ const sendButton = document.getElementById('send-button');
 const messageList = document.getElementById('messageList');
 const messageListEmpty = document.getElementById('messageListEmpty');
 
+sendButton.addEventListener('click', () => {
+  const message = messageInput.value;
+  if (message === '/help') {
+    displayMessage(`
+    <p style="color: #af9cff;">System Bot: Hi there! How may I help you?</p>
+    <ul style="color: #af9cff; font-family: "VT323";">
+    <li>/help about ---> What is Massroom?</li>
+    <li>/help account ---> Info about your account (WILL NOT BE SHOWN PUBLICLY)</li>
+    <li>/help faq ---> Common questions about Massroom</li>
+    <li>/help html ---> How to send SVGs, clickable links, and even styled text!</li>
+    </ul>
+    `);
+  };
+  //mainChannel.message();
+});
