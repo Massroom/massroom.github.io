73ca8b9dd6b75680bbe966a74a802c0502313d19
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-21 19:34:46 +0800

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -9,9 +9,9 @@ sendButton.addEventListener('click', () => {
     <p style="color: #af9cff;">System Bot: Hi there! How may I help you?</p>
     <ul style="color: #af9cff; font-family: "VT323";">
     <li>/help about ---> What is Massroom?</li>
+    <li>/help text ---> How to send SVGs, clickable links, and even styled text!</li>
     <li>/help account ---> Info about your account (WILL NOT BE SHOWN PUBLICLY)</li>
     <li>/help faq ---> Common questions about Massroom</li>
-    <li>/help html ---> How to send SVGs, clickable links, and even styled text!</li>
     </ul>
     `);
   } else if (message === '/help about') {
@@ -20,6 +20,25 @@ sendButton.addEventListener('click', () => {
     <p style="color: #af9cff;">System Bot: Massroom is a chatroom app made for large groups of people on a public chat! Its conpletely free to use (and we built it 100% free too), its also open source and on GitHub!</p>
     <a href="https://github.com/massroom/">View it here!</a>
     `);
-  };
+  } else if (message === '/help text') {
+    console.log('bot detected help');
+    displayMessage(`
+    <p>You can display HTML messages by sending HTML code over the input bar. Below are some examples:</p>
+    <ul style="color: #af9cff; font-family: "VT323";">
+    <li>
+    <p>Clickable Links:</p>
+    <code>
+    &lt;a href="your-link-here"&gt;Link&lt;/a&gt;</code>
+    </li>
+    <li>
+    <p>SVGs:</p>
+    <code>
+    &lt;svg&gt;*svg code here*&lt;/svg&gt;</code>
+    </li>
+    </ul>
+    `);
+  } else if (message === '/help account') {
+    
+  }
   //mainChannel.message();
 });
