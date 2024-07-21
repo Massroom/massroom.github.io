f33a3a1022c3a6123ee3900f2f1307335bce0ef7
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-21 15:38:49 +0800

    Create bot.js

---
+++ js/bot.js
@@ -0,0 +1,5 @@
+const messageInput = document.getElementById('message-input');
+const sendButton = document.getElementById('send-button');
+const messageList = document.getElementById('messageList');
+const messageListEmpty = document.getElementById('messageListEmpty');
+
