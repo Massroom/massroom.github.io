7e225b58da1451887498b61db85513255c3a09e9
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-21 19:18:36 +0800

    Update bot.js

--- js/bot.js
+++ js/bot.js
@@ -14,8 +14,7 @@ sendButton.addEventListener('click', () => {
     <li>/help html ---> How to send SVGs, clickable links, and even styled text!</li>
     </ul>
     `);
-  };
-  else if (message === '/help about') {
+  }; else if (message === '/help about') {
     console.log('bot detected help');
     displayMessage(`
     <p style="color: #af9cff;">System Bot: Massroom is a chatroom app made for large groups of people on a public chat! Its conpletely free to use (and we built it 100% free too), its also open source and on GitHub!</p>
