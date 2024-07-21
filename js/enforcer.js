99673c35729c93104890a7e22c89f271b456692f
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-21 17:31:39 +0800

    Update enforcer.js

--- js/enforcer.js
+++ js/enforcer.js
@@ -4,7 +4,7 @@ sendButton.addEventListener('click', () => {
   const message = messageInput.value;
   if (message.includes('fuck' || 'nigger' || 'shit')) {
     console.log('enforcer activated');
-    displayMessage(`<h2 style="color: #af9cff;">Warning: @${username}We do not encourage swearing in Massroom. Please stop that.</h2>`);
+    displayMessage(`<h4 style="color: #fff000;">Warning: @${username}We do not encourage swearing in Massroom. Please stop that.</h4>`);
   };
   if (message.includes('suicide' || 'kys' || 'kill yourself')) {
     console.log('enforcer activated');
