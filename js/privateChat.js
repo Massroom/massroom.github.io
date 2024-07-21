61eb503a4bd7d0fcf085463c094d662bb348d9a2
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-21 09:58:31 +0800

    Update privateChat.js

--- js/privateChat.js
+++ js/privateChat.js
@@ -20,11 +20,50 @@ function generatePrivateChatId() {
 }
 
 
+
 const xanoClient = new XanoClient({
   instanceBaseUrl: "https://x8ki-letl-twmt.n7.xano.io/",
   realtimeConnectionHash: "yHjeFpItI_ivBM9KL-tjZSYQAwM",
 });
 
+if (auth === 1) {
+  const setAuth = localStorage.getItem("authToken");
+  xano.setRealtimeAuthToken(authToken);
+}
+
+function generateRandomUsername() {
+  const adjectives = ['Awesome', 'Bold', 'Crazy', 'Daring', 'Energetic', 'Stealthy', 'Quick', 'Fierce'];
+  const nouns = ['Lion', 'Tiger', 'Bear', 'Wolf', 'Dragon', 'Fox', 'Serpent', 'Elephant'];
+  const randomAdjective = adjectives[Math.floor(Math.random() * adjectives.length)];
+  const randomNoun = nouns[Math.floor(Math.random() * nouns.length)];
+  return `${randomAdjective}${randomNoun}`;
+}
+
+var username
+
+if (auth == 1) {
+  fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/me', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Authorization': 'Bearer ' + localStorage.getItem('authToken') // Use the stored JWT
+      }
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      username = data.name
+      // Set the innerHTML of the created HTML elements
+      //document.getElementById('username-display').innerHTML = data.name;
+      //document.getElementById('password-display').innerHTML = data.email;
+      //document.getElementById('timestamp').innerHTML = data.created_at; //'Account created at: ' + data.created_at;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+}
+else {
+  username = generateRandomUsername();
+}
 
 const privateChatId = generatePrivateChatId();
 // Add an input field for the chat ID
