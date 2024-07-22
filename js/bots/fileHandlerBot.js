ac8cee57821c957efe2ad77ca413580a57d6e346
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-22 21:37:49 +0800

    Update fileHandlerBot.js

--- js/bots/fileHandlerBot.js
+++ js/bots/fileHandlerBot.js
@@ -7,26 +7,15 @@ sendButton.addEventListener('click', () => {
   if (message.startsWith('/file send')) {
     const fileUrl = message.slice('/file send'.length).trim();;
     const fileName = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
-    
-    fetch(fileUrl)
-      .head()
-      .then(response => {
-        const fileSize = response.headers.get('Content-Length');
-        const fileExt = fileName.substring(fileName.lastIndexOf('.') + 1);
-
-        const fileHTML = `
-        <a href="${fileUrl}" download>
-        <button class="btn btn-dark btn-sm">
-        <span class="file-info">
-        <span class="file-size">${(fileSize / 1024).toFixed(2)} KB</span>
-        <span class="file-ext">.${fileExt}</span>
-        </span>
-        Download File
-        <i class="fas fa-download"></i>
-        </button>
-        </a>
-        `;
-        displayMessage(fileHTML);
-      });
+    const fileHTML = `
+    <a href="${fileUrl}" download>
+    <button class="btn btn-dark btn-sm">
+    Download ${fileName}
+    <i class="fas fa-download"></i>
+    </button>
+    </a>
+    `;
+    displayMessage(fileHTML);
+  });
   }
 });
