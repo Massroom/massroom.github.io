b8819d92c16ee3f9c92f926af29b1051243318b9
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-22 21:31:21 +0800

    Update fileHandlerBot.js

--- js/bots/fileHandlerBot.js
+++ js/bots/fileHandlerBot.js
@@ -1,24 +1,32 @@
-  const fileUrl = message.file_url;
-  const fileName = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
+// init bot
 
-  fetch(fileUrl)
-    .head()
-    .then(response => {
-      const fileSize = response.headers.get('Content-Length');
-      const fileExt = fileName.substring(fileName.lastIndexOf('.') + 1);
+sendButton.addEventListener('click', () => {
+  console.log('bot ready');
+  const message = messageInput.value;
+  console.log('bot read: ' + message);
+  if (message.startsWith('/file send')) {
+    const fileUrl = message.slice('/file send'.length).trim();;
+    const fileName = fileUrl.substring(fileUrl.lastIndexOf('/') + 1);
+    
+    fetch(fileUrl)
+      .head()
+      .then(response => {
+        const fileSize = response.headers.get('Content-Length');
+        const fileExt = fileName.substring(fileName.lastIndexOf('.') + 1);
 
-      const fileHTML = `
+        const fileHTML = `
         <a href="${fileUrl}" download>
-          <button class="btn btn-dark btn-sm">
-            <span class="file-info">
-              <span class="file-size">${(fileSize / 1024).toFixed(2)} KB</span>
-              <span class="file-ext">.${fileExt}</span>
-            </span>
-            Download File
-            <i class="fas fa-download"></i>
-          </button>
+        <button class="btn btn-dark btn-sm">
+        <span class="file-info">
+        <span class="file-size">${(fileSize / 1024).toFixed(2)} KB</span>
+        <span class="file-ext">.${fileExt}</span>
+        </span>
+        Download File
+        <i class="fas fa-download"></i>
+        </button>
         </a>
-      `;
-
-      displayMessage(fileHTML);
-    });
+        `;
+        displayMessage(fileHTML);
+      });
+  }
+});
