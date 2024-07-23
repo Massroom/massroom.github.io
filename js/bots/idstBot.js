56adda6fcff7d1eedd42a7fecd2ac9d3b99118aa
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-23 17:41:43 +0800

    Create idstBot.js

---
+++ js/bots/idstBot.js
@@ -0,0 +1,6 @@
+function idstBot() {
+  const message = messageInput.value;
+  if (message.includes('/1dst')) {
+    mainChannel.message(`<p style="color: #0062ff; font-family: "VT323";">1D10T1C STUD10S IS COOL!</p>`);
+  };
+};
