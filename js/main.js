2666ed6ef593413d710dd53f1b570c57e8fea82c
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-23 22:56:23 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -159,7 +159,7 @@ function getUserRole() {
         console.error(error);
       });
   } else {
-    console.log('user is not signed in');
+    console.log('user is not signed in, cannot get role');
     return null;
   }
 };
@@ -180,8 +180,11 @@ sendButton.addEventListener('click', () => {
       size = 14;
       icon = 'M22 2H2v12h2V4h16v10h2V2zM6 14H4v2h2v-2zm0 2h2v2h2v2H8v-2H6v-2zm4 4v2h4v-2h2v-2h-2v2h-4zm10-6h-2v2h-2v2h2v-2h2v-2z';
     };
+    console.log(role);
+    console.log(icon);
+    console.log(size);
     mainChannel.message(`
-    <div style="display: flex; align-items: center; gap: 10px;">
+    <div style="display: flex; align-items: center; gap: 6px;">
       <svg width="${size}" height="${size}" style="fill: #00f52d;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24">
         <path d="${icon}" />
       </svg>
