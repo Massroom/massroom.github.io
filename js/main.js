4af99fe1f80e5734dcfe97c32d429c1686f042cd
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-23 23:00:48 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -173,10 +173,10 @@ sendButton.addEventListener('click', () => {
     if (role === null) {
       icon = null;
       size = 0;
-    } else if (role === 'developer') {
+    } else if (role === 'Developer') {
       size = 14;
       icon = 'M8 5h2v2H8V5zM6 7h2v2H6V7zM4 9h2v2H4V9zm-2 2h2v2H2v-2zm2 2h2v2H4v-2zm2 2h2v2H6v-2zm2 2h2v2H8v-2zm8-12h-2v2h2V5zm2 2h-2v2h2V7zm2 2h-2v2h2V9zm2 2h-2v2h2v-2zm-2 2h-2v2h2v-2zm-2 2h-2v2h2v-2zm-2 2h-2v2h2v-2z';   
-    } else if (role === 'moderator') {
+    } else if (role === 'Moderator') {
       size = 14;
       icon = 'M22 2H2v12h2V4h16v10h2V2zM6 14H4v2h2v-2zm0 2h2v2h2v2H8v-2H6v-2zm4 4v2h4v-2h2v-2h-2v2h-4zm10-6h-2v2h-2v2h2v-2h2v-2z';
     };
