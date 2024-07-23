bb7df90cf0591f2a11075e73dc11c6347062e46f
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-23 22:42:24 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -148,9 +148,10 @@ sendButton.addEventListener('click', () => {
   setTimeout(function() {
     const message = messageInput.value;
     checkForSpam(message); // Execute spam detection
+    let icon = null;
     role = getUserRole();
     if (role === null) {
-      icon = '';
+      icon = null;
     } else if (role === 'developer') {
       icon = 'M8 5h2v2H8V5zM6 7h2v2H6V7zM4 9h2v2H4V9zm-2 2h2v2H2v-2zm2 2h2v2H4v-2zm2 2h2v2H6v-2zm2 2h2v2H8v-2zm8-12h-2v2h2V5zm2 2h-2v2h2V7zm2 2h-2v2h2V9zm2 2h-2v2h2v-2zm-2 2h-2v2h2v-2zm-2 2h-2v2h2v-2zm-2 2h-2v2h2v-2z';   
     } else if (role === 'moderator') {
