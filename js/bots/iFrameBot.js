6ffadc54c91431d70ef2d12edebcbe37a6cc9dc8
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-25 16:20:33 +0800

    Update iFrameBot.js

--- js/bots/iFrameBot.js
+++ js/bots/iFrameBot.js
@@ -1,10 +1,14 @@
 function iFrameBot() {
   const message = messageInput.value;
-  if (message.startsWith('/embed') && role != null) {
-    const url = message.slice('/embed'.length).trim();;
-    const html = `
-    <iframe width="99%" height="240" src="${url}" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
-    `;
-    mainChannel.message(html);
+  if (message.startsWith('/embed')) {
+    if (role != null) {
+      const url = message.slice('/embed'.length).trim();;
+      const html = `
+      <iframe width="99%" height="240" src="${url}" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
+      `;
+      mainChannel.message(html);
+    } else {
+      displayMessage(`<p style="color: #ff0000;">@${username} this is a paid feature. Use premium to be able to /embed!</p>`);
+    }
   };
 };
