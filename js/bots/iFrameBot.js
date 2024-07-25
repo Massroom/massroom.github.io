761adfdfc67313fd5ecfb7fd68f7c766793a075f
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-25 16:13:21 +0800

    Update iFrameBot.js

--- js/bots/iFrameBot.js
+++ js/bots/iFrameBot.js
@@ -1,6 +1,6 @@
 function iFrameBot() {
   const message = messageInput.value;
-  if (message.startsWith('/embed')) {
+  if (message.startsWith('/embed') && role != null) {
     const url = message.slice('/embed'.length).trim();;
     const html = `
     <iframe width="99%" height="240" src="${url}" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
