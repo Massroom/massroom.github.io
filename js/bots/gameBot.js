45900a02e638fa1ba0dd9f9ead65e66304b834d5
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-26 13:28:11 +0800

    Update gameBot.js

--- js/bots/gameBot.js
+++ js/bots/gameBot.js
@@ -15,7 +15,12 @@ function gameBot() {
         <iframe width="99%" height="240" src="https://scriptorite.proj.sbs/interactives/flappybird4.html" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
         `;
         mainChannel.message(html);
-      };
+      } else if (message === '/game platformer') {
+        const html = `
+        <iframe width="99%" height="240" src="https://massroom.js.org/exe/platformer" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
+        `;
+        mainChannel.message(html);
+      }
     } else {
       displayMessage(`<p style="color: #ff0000;">@${username} error fetching your role. Try the command again.</p>`);
     };
