4c15fd1912d1f7d0016b8151c011acb3f21ca60c
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-26 10:58:02 +0800

    Update gameBot.js

--- js/bots/gameBot.js
+++ js/bots/gameBot.js
@@ -1,16 +1,18 @@
 function gameBot() {
   const message = messageInput.value;
-  if (role == 'Member') {
-    displayMessage(`<p style="color: #ff0000;">@${username} this is a paid feature. Use premium to be able to /play!</p>`);
-  } else if (role != null) {
-    if (message === '/game pacman') {
-      const url = message.slice('/embed'.length).trim();;
-      const html = `
-      <iframe width="99%" height="240" src="https://massroom.js.org/exe/pacman" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
-      `;
-      mainChannel.message(html);
-    };  
-  } else {
-    displayMessage(`<p style="color: #ff0000;">@${username} error fetching your role. Try the command again.</p>`);
+  if (message.startsWith('/game')) 
+    if (role == 'Member') {
+      displayMessage(`<p style="color: #ff0000;">@${username} this is a paid feature. Use premium to be able to /play!</p>`);
+    } else if (role != null) {
+      if (message === '/game pacman') {
+        const url = message.slice('/embed'.length).trim();;
+        const html = `
+        <iframe width="99%" height="240" src="https://massroom.js.org/exe/pacman" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
+        `;
+        mainChannel.message(html);
+      };  
+    } else {
+      displayMessage(`<p style="color: #ff0000;">@${username} error fetching your role. Try the command again.</p>`);
+    };
   };
 };
