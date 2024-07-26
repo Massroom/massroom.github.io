e86ce930050ada631d7e6969f15930bc28c1b611
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-26 18:06:24 +0800

    Update gameBot.js

--- js/bots/gameBot.js
+++ js/bots/gameBot.js
@@ -2,7 +2,7 @@ function gameBot() {
   const message = messageInput.value;
   if (message.startsWith('/game')) {
     if (role == 'Member') {
-      displayMessage(`<p style="color: #ff0000;">@${username} this is a paid feature. Use premium to be able to /play!</p>`);
+      displayMessage(`<p style="color: #ff0000;">@${username} this is a paid feature. Use premium to be able to /game!</p>`);
     } else if (role != null) {
       if (message === '/game pacman') {
         const url = message.slice('/embed'.length).trim();;
@@ -20,7 +20,12 @@ function gameBot() {
         <iframe width="99%" height="240" src="https://massroom.js.org/exe/platformer" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
         `;
         mainChannel.message(html);
-      }
+      } else if (message === '/game cubersurge') {
+        const html = `
+        <iframe width="99%" height="240" src="https://cubersurge.github.io/play/builds/CubersurgeAlpha1-2.html" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
+        `;
+        mainChannel.message(html);
+      };
     } else {
       displayMessage(`<p style="color: #ff0000;">@${username} error fetching your role. Try the command again.</p>`);
     };
