d811eac4ee67507d0754d0394a892d7a8ba96e1b
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-27 13:47:59 +0800

    Update gameBot.js

--- js/bots/gameBot.js
+++ js/bots/gameBot.js
@@ -25,6 +25,11 @@ function gameBot() {
         <iframe width="99%" height="240" src="https://cubersurge.github.io/play/builds/CubersurgeAlpha1-2.html" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
         `;
         mainChannel.message(html);
+      } else if (message === '/game 3d') {
+        const html = `
+        <iframe width="99%" height="240" src="https://massroom.js.org/exe/3d" frameborder="0" allowfullscreen style="padding-top: 10px; border-radius: 12px;"></iframe>
+        `;
+        mainChannel.message(html);
       };
     } else {
       displayMessage(`<p style="color: #ff0000;">@${username} error fetching your role. Try the command again.</p>`);
