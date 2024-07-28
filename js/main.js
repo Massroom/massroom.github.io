0c9b7fc3a3912e5de78d5d48ba048efcbdd2a621
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-29 07:30:12 +0800

    Update main.js

--- js/main.js
+++ js/main.js
@@ -253,7 +253,18 @@ sendButton.addEventListener('click', () => {
         <p>${username}: ${message}</p>
       </div>
       `;
-    }  else if (role === 'Stupid') {
+    } else if (role === 'Supporter') {
+      html = `
+      <div style="display: flex; align-items: center; gap: 3px;">
+      <div aria-label="Early Supporter" data-balloon-pos="up">
+        <svg width="16" height="16" style="fill: #00f52d;" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" aria-label="Premium Member" data-balloon-pos="up">
+          <path d="M4 2H2v8h2V2zm16 0h2v8h-2V2zm-6 6h-4V2H4v2h4v4H4v2h4v4H4v2h4v4H4v2h6v-6h4v6h2v-6h4v-2h-4v-4h4V8h-4V2h-2v6zm-4 6v-4h4v4h-4zM20 2h-4v2h4V2zM2 14h2v8H2v-8zm14 6h4v2h-4v-2zm6-6h-2v8h2v-8z">
+        </svg>
+      </div>
+        <p>${username}: ${message}</p>
+      </div>
+      `;
+    } else if (role === 'Stupid') {
       html = `
       <div style="display: flex; align-items: center; gap: 3px;">
       <div aria-label="Make fun of this user NOW!" data-balloon-pos="up">
