c801c68d7bcf4bde5841501338cefb1224dcb975
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-28 16:45:48 +0800

    Update voiceChat.js

--- js/voiceChat.js
+++ js/voiceChat.js
@@ -4,6 +4,55 @@ const callButton = document.getElementById('call-button');
 const remoteIdInput = document.getElementById('remote-id');
 const peerIdDisplay = document.getElementById('peer-id');
 const userList = document.getElementById('user-list');
+const container = document.getElementById("container");
+const canvas = document.getElementById("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const ctx = canvas.getContext("2d");
+const audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+let audioSource = null;
+let analyser = null;
+
+audioSource = audioCtx.createMediaElementSource(audio1);
+analyser = audioCtx.createAnalyser();
+audioSource.connect(analyser);
+analyser.connect(audioCtx.destination);
+
+analyser.fftSize = 128;
+const bufferLength = analyser.frequencyBinCount;
+const dataArray = new Uint8Array(bufferLength);
+const barWidth = canvas.width / bufferLength;
+
+let x = 0;
+function animate() {
+    x = 0;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    analyser.getByteFrequencyData(dataArray);
+    drawVisualizer({
+        bufferLength,
+        dataArray,
+        barWidth
+    });
+    requestAnimationFrame(animate);
+}
+
+const drawVisualizer = ({
+    bufferLength,
+    dataArray,
+    barWidth
+}) => {
+    let barHeight;
+    for (let i = 0; i < bufferLength; i++) {
+        barHeight = dataArray[i];
+        const red = (i * barHeight) / 10;
+        const green = i * 4;
+        const blue = barHeight / 4 - 12;
+        ctx.fillStyle = `rgb(${red}, ${green}, ${blue})`;
+        ctx.fillRect(x, canvas.height - barHeight, barWidth, barHeight);
+        x += barWidth;
+    }
+};
 
 let peer;
 let connections = {};
