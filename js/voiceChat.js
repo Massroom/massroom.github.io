5fa7bbad84cae5ce6b23a49a7faaef23c3508624
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-28 09:50:16 +0800

    Update voiceChat.js

--- js/voiceChat.js
+++ js/voiceChat.js
@@ -1,1 +1,28 @@
 const peer = new Peer('massroom-user');
+
+peer.on('connection', (conn) => {
+  console.log('Connected to peer!');
+  // Handle voice chat data here
+});
+
+conn.on('data', (data) => {
+  console.log('Received voice chat data:', data);
+  // Play the received audio data here
+});
+
+const audioData = // Get audio data from user's microphone
+conn.send(audioData);
+
+navigator.mediaDevices.getUserMedia({ audio: true })
+  .then((stream) => {
+    const audioContext = new AudioContext();
+    const source = audioContext.createMediaStreamSource(stream);
+    const processor = audioContext.createScriptProcessor(1024, 1, 1);
+    source.connect(processor);
+    processor.connect(audioContext.destination);
+    processor.onaudioprocess = (event) => {
+      const audioData = event.inputBuffer.getChannelData(0);
+      // Send audioData to peer using conn.send(audioData)
+    };
+  })
+  .catch((error) => console.error('Error getting user media:', error));
