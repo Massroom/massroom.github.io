216278ec096d21ce59487afc3f2a2fbf8faaba6d
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-29 15:34:30 +0800

    Create 1dst.js

---
+++ js/rooms/1dst.js
@@ -0,0 +1,5 @@
+function idst() {
+  if (privateChatId === '1dst') {
+    messageListEmpty.insertAdjacentHTML('beforeend', '<p style="color: #af9cff;">Info: Welcome to 1DST updates channel! Check in for any new 1DST updates here!</p>');
+  }
+}
