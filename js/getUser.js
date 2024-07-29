24bcfd5338c747d5e6f7fb51736c165ae73b0a11
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-29 17:45:37 +0800

    Update getUser.js

--- js/getUser.js
+++ js/getUser.js
@@ -1,15 +1,21 @@
-fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/getuser', {
-  method: 'GET',
-  headers: {
-    'Content-Type': 'application/json'
-  },
-  body: JSON.stringify({
-    username: username,
-  })
-})
-  .then((response) => {
-    if (!response.ok) { // Check if the response status is not ok (200-299)
-      throw new Error('Failed: ' + response.status);
-    }
+function getUser(user) {
+  fetch('https://x8ki-letl-twmt.n7.xano.io/api:iGbUspz7/auth/getuser', {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json'
+    },
+    body: JSON.stringify({
+      username: user,
+    });
+  });
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error('Failed: ' + response.status);
+      };
     return response.json();
-  })
+    });
+    .then((data) => {
+      document.getElementById('username-display').innerHTML = data.name;
+      document.getElementById('timestamp').innerHTML = data.massroom_role;
+    });
+};
