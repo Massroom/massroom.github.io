b7333e9daeef270fa771137c645a39ddd5c68264
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-07-30 22:13:28 +0800

    Update privateChat.js

--- js/privateChat.js
+++ js/privateChat.js
@@ -185,7 +185,7 @@ function updateAuthStatus() {
 
 function idst() {
   if (privateChatId === '1dst') {
-    messageListEmpty.insertAdjacentHTML('beforeend', '<p style="color: #af9cff;">Info: Welcome to 1DST updates channel! Check in for any new 1DST updates here!</p>');
+    startMessage.insertAdjacentHTML('beforeend', '<p style="color: #af9cff;">Info: Welcome to 1DST updates channel! Check in for any new 1DST updates here!</p>');
   }
 }
 
