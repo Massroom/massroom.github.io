76ad924062eb46f9da19a3180618b521211122a8
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2024-08-03 12:20:07 +0800

    Update voiceChat.js

--- js/voiceChat.js
+++ js/voiceChat.js
@@ -6,12 +6,26 @@ const peerIdDisplay = document.getElementById('peer-id');
 const userList = document.getElementById('user-list');
 const transcriptionDiv = document.getElementById('transcription');
 
+function generatePrivateChatId() {
+  n1 = Math.floor((Math.random() * 9) + 1);
+  n2 = Math.floor((Math.random() * 9) + 1);
+  n3 = Math.floor((Math.random() * 9) + 1);
+  n4 = Math.floor((Math.random() * 9) + 1);
+  n5 = Math.floor((Math.random() * 9) + 1);
+  n6 = Math.floor((Math.random() * 9) + 1);
+  n7 = Math.floor((Math.random() * 9) + 1);
+  n8 = Math.floor((Math.random() * 9) + 1);
+  n9 = Math.floor((Math.random() * 9) + 1);
+  return `${n1}${n2}${n3}${n4}${n5}${n6}${n7}${n8}${n9}`
+}
+
 let peer;
 let connections = {};
+let genId = generatePrivateChatId();
 
 // Create a PeerJS instance
 function initializePeer() {
-    peer = new Peer();
+    peer = new Peer(genId);
 
     // Display your peer ID
     peer.on('open', id => {
