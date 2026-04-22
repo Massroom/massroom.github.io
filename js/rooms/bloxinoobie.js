c7e20eae654338678f519b7fa98a57571722bc69
Dev10us <112738649+melonmasteristaken@users.noreply.github.com>
2026-04-22 12:01:04 +0800

    watch out

---
+++ js/rooms/bloxinoobie.js
@@ -0,0 +1,5 @@
+function bloxinoobie() {
+    if (privateChatId === 'bloxinoobie') {
+        messageListEmpty.insertAdjacentHTML('beforeend', '<p style="color: #af9cff;">Info: Bloxinoobie is very cool guys (Trust trust)</p> <p style="color: #af9cff;">Boxinoobie thinks he is better than Bloxinoobie but we all know it\'s not true.</p>')
+    }
+}
